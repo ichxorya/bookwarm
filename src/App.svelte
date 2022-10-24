5b1e41e0eb54d70d7da6a3f5d3679617b54b3c5f
ichxorya <benghia1st@gmail.com>
2022-10-24 12:49:13 +0700

    v1.3: Noon push

--- src/App.svelte
+++ src/App.svelte
@@ -1,59 +1,42 @@
 <script>
   import logo from './assets/logos/bookwarm.png'
   import themesong from './assets/sounds/Ching Cheng Hanji - Peng Lexer Remix.mp3'
-  import Counter from './lib/Counter.svelte'
-  import Newsfeed from './routes/newsfeed/+page.svelte'
 
-  let bgImage = './assets/backgrounds/book_burning_wiki.jpg'
+  import { Router, Route, Link } from 'svelte-navigator'
 </script>
 
 <main>
+  <!--Favicon-->
   <link rel="icon" href={logo} />
   <div>
     <img src={logo} class="logo bookwarm" alt="Bookwarm Logo" />
   </div>
 
+  <!--Background-->
+  <div class="background"></div>
+
+  <!--Theme Song-->
   <div>
     <audio controls autoplay>
       <source src={themesong} type="audio/mpeg">
     </audio>
     <h5 style="text-align: left">(っ◔◡◔)っ ♥ ~một bản nhạc lofi nhẹ nhàng~ ♥</h5>
-    <h5 style="text-align: right">trước khi bạn khám phá thế giới kì diệu của...</h5>
+    <h5 style="text-align: right">( ͡° ͜ʖ ͡°) ♥ ~trước khi bạn khám phá thế giới kì diệu của...~ ♥</h5>
   </div>
 
-  <h1>🅱𝕠ⓞ🅺ⓦ🅐Ɽ🄼</h1>
+  <!--Index Page-->
+  <div id="index-name">
+    🔥📚🔥<name>🅱𝕠ⓞ🅺ⓦ🅐Ɽ🄼</name>🔥📚🔥
+  </div>
   <h2>🅱a tôi👨 nói👄, có 3️⃣ thứ quan trọng ❗ nhất 👍 trong cuộc đời🧬 bạn🙏:</h2>
   <h3>Ba👨🏽 của bạn là ai❔😤😤😤</h3>
   <h3>&emsp &emsp &emsp &emsp Bố👨🏻 của bạn là ai❓🥺🥺🥺</h3>
   <h3>&emsp &emsp &emsp &emsp &emsp &emsp &emsp &emsp Và sách📚 của bạn❓❔❓❔🧠🧠🧠</h3>
   <h2>Với tôi, sách vừa là 🅱ạn, vừa là 🅱ố*!</h2>
   <h6 style="text-align: right">Bố = Thầy😲😲😲</h6>
-
-  <div class="card">
-    <Counter/>
-  </div>
-
 </main>
 
 <style>
-  .card {
-    background: #fff;
-    border-radius: 3px;
-    box-shadow: 0 1px 3px rgba(0, 0, 0, 0.12), 0 1px 2px rgba(0, 0, 0, 0.24);
-    display: flex;
-    flex-direction: column;
-    justify-content: center;
-    align-items: center;
-    padding: 1em;
-    margin: 0 auto;
-    max-width: 240px;
-    transition: all 0.3s cubic-bezier(.25, .8, .25, 1);
-  }
-
-  .card:hover {
-    box-shadow: 0 14px 28px rgba(0, 0, 0, 0.25), 0 10px 10px rgba(0, 0, 0, 0.22);
-  }
-
   .logo {
     animation: App-logo-spin infinite 4s linear;
     height: 6em;

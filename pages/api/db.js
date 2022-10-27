b6270d546cacf989a875f90a680de71302c62ffb
ichxorya <benghia1st@gmail.com>
2022-10-28 02:05:08 +0700

    v2.03: Database connected!!!

---
+++ pages/api/db.js
@@ -0,0 +1,24 @@
+import mysql from "mysql2/promise";
+
+let DB_NAME = "exampledb";
+
+export default async function handler(req, res) {
+  const dbconnection = await mysql.createConnection({
+    host: process.env.DB_HOST,
+    database: DB_NAME,
+    port: process.env.DB_PORT,
+    user: process.env.DB_USER,
+    password: process.env.DB_PASSWORD,
+  });
+  try {
+    const query = "SELECT * FROM cunny";
+    const values = [];
+    const [data] = await dbconnection.execute(query, values);
+    dbconnection.end();
+
+    res.status(200).json({ products: data });
+  } catch (error) {
+    // unhide to check error
+    // res.status(500).json({ error: error.message });
+  }
+}
